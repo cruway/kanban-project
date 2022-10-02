f9d1273eb47b35c083cf8e4701f44821ffb12ed0
sim wook <astralkaiser89@gmail.com>
2022-10-02 21:52:22 +0900

    drag and drop update

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,77 @@
-import React from 'react';
-import {useRecoilState, useRecoilValue} from "recoil";
-import {hourSelector, minutesState} from "./atoms";
+import {DragDropContext, Draggable, Droppable, DropResult} from "react-beautiful-dnd";
+import styled from "styled-components";
+import {toDoState} from "./atoms";
+import {useRecoilState} from "recoil";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+export const Board = styled.div`
+  padding: 30px 10px;
+  background-color: ${props => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${props => props.theme.cardColor};
+`;
 
 function App() {
-    const [minutes, setMinutes] = useRecoilState(minutesState);
-    const [hours, setHours] = useRecoilState(hourSelector);
-    const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
-        setMinutes(+event.currentTarget.value);
-    };
-    const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
-        setHours(+event.currentTarget.value);
+    const [toDos, setToDos] = useRecoilState(toDoState);
+    const onDragEnd = ({draggableId, destination, source}:DropResult) => {
+        if(!destination) return;
+       setToDos(oldToDos => {
+           const copyToDos = [...oldToDos];
+           // 1) Delete item on source.index
+           copyToDos.splice(source.index, 1);
+           // 2) Put back the item on the destination.index
+           copyToDos.splice(destination?.index, 0, draggableId);
+           return copyToDos;
+       })
     };
     return (
-        <div>
-          <input
-              value={minutes}
-              onChange={onMinutesChange}
-              type={"number"}
-              placeholder={"Minutes"}
-          />
-          <input
-              onChange={onHoursChange}
-              value={hours}
-              type={"number"}
-              placeholder={"Hours"}
-          />
-        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Wrapper>
+                <Boards>
+                    <Droppable droppableId={"one"}>
+                        {(magic) => (
+                            <Board ref={magic.innerRef} {...magic.droppableProps}>
+                                {toDos.map((toDo, index) => (
+                                    <Draggable key={toDo} draggableId={toDo} index={index}>
+                                        {(magic) => (
+                                            <Card
+                                                ref={magic.innerRef}
+                                                {...magic.draggableProps}
+                                                {...magic.dragHandleProps}>
+                                                {toDo}
+                                            </Card>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {magic.placeholder}
+                            </Board>
+                            )}
+                        </Droppable>
+                </Boards>
+            </Wrapper>
+        </DragDropContext>
     );
 }
 
